0b8cb48a6a68a9983aa641a705e9788f4b7c0f4e
Nicholas Allam <njallam@outlook.com>
2016-08-03 11:09:36 +0100

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,83 @@
+//Values
+xp = {
+  "Quick Play": {
+    "Base XP": 3.4,
+    "Bronze": 50,
+    "Silver": 100,
+    "Gold": 150,
+    "Match Finished": 250,
+    "Win": 500,
+    "Consecutive Match": 200,
+    "Backfill": 400,
+    "First Win of the Day": 1500
+  },
+  "Practice vs. AI": {
+    "Base XP": 2.015,
+    "Bronze": 30,
+    "Silver": 60,
+    "Gold": 90,
+    "Match Finished": 150,
+    "Win": 300,
+    "Consecutive Match": 120,
+    "Backfill": 240,
+    "First Win of the Day": 1500
+  },
+  "Weekly Brawl": {
+    "Base XP": 3.025,
+    "Bronze": 45,
+    "Silver": 90,
+    "Gold": 135,
+    "Match Finished": 225,
+    "Win": 450,
+    "Consecutive Match": 180,
+    "Backfill": 240, //?
+    "First Win of the Day": 1350
+  }
+};
+
+//Selectors
+mode = 'select[name="mode"]';
+timeplayed = 'input[name="timeplayed"]';
+basexp = 'input[name="basexp"]';
+medal = 'input[name="medal"]';
+medalc = medal+":checked"
+medalxp = 'input[name="medalxp"]';
+flags = 'input[name="flags"]';
+flagsc = flags+":checked";
+bonusxp = 'input[name="bonusxp"]';
+group = 'input[name="group"]';
+groupxp = 'input[name="groupxp"]';
+totalxp = 'input[name="totalxp"]';
+inputs = [mode, timeplayed, medal, flags, group].join();
+
+//Event
+$(inputs).change(calculate);
+
+//Functions
+function calculate() {
+    var mod = $(mode).val()
+    //base xp
+    var bas = timeToSeconds($(timeplayed).val()) * xp[mod]["Base XP"]
+    $(basexp).val(bas);
+    //medal xp
+    var med = ($(medalc).val() != "None") ? xp[mod][$(medalc).val()] : 0
+    $(medalxp).val(med);
+    //bonus xp
+    var bon = 0;
+    $(flagsc).each(function() {bon += xp[mod][$(this).val()] });
+    $(bonusxp).val(bon);
+    //group xp
+    var sum = bas + med + bon
+    var grp = $(group).is(':checked') ? sum * 0.2 : 0
+    $(groupxp).val(grp);
+    //total xp
+    var tot = sum + grp
+    $(totalxp).val(tot);
+}
+
+function timeToSeconds(str) {
+  var p = str.split(':').map(Number);
+  return p[0]*60 + p[1];
+}
+
+calculate();
